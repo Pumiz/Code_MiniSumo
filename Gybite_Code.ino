a171f76a325db9ad34760c59ea47653c85aa8390
Martín Gomez Valle <111396330+Pumiz@users.noreply.github.com>
2022-10-21 23:22:25 -0300

    Update Gybite_Code.ino
    Actualice el codigo de Gybite para la fecha de La Salle. Estrategia uno funciona.

--- Gybite_Code.ino
+++ Gybite_Code.ino
@@ -1,6 +1,6 @@
 
 //--------------FUNCIONES GLOBALES---------------
-#define DEBUG 1
+#define DEBUG 0
 #define ON HIGH
 #define OFF LOW
 
@@ -13,20 +13,20 @@
 #define PWM_MOTOR_IZQ 11
 
 int Pines_Motor[4] = {
-    MOTOR_DER_1,
-    MOTOR_DER_2,
-    MOTOR_IZQ_1,
-    MOTOR_IZQ_2,
+  MOTOR_DER_1,
+  MOTOR_DER_2,
+  MOTOR_IZQ_1,
+  MOTOR_IZQ_2,
 };
 
-#define SPEED_SERCHING 100
-#define SPEED_ATTACK 255
+#define VELOCIDAD_BUSCAR 100
+#define VELOCIDAD_ATAQUE 200
 
-//-------------------PULSADOR--------------------
+//-------------------PULSADOR-- C-------------
 #define PIN_PULSADOR A3
 #define PIN_LED 13
 bool pantalla = true;
-unsigned long tiempo_max = 10000;
+unsigned long tiempo_max = 3000;
 int tiempo;
 
 enum ESTADOS
@@ -43,123 +43,69 @@ bool pulsador;
 int estado = SET1;
 int estado_anterior = SET3;
 
+
 //------------------ULTRASONIDO------------------
 #define PIN_TRIG 5
 #define PIN_ECHO 6
 long Tiempo;
 long Distancia;
 
-//--------------------SHARP´S--------------------
-#define PIN_SHARP_F_D A7
-#define PIN_SHARP_F_I A6
-#define PIN_SHARP_DER A5
-#define PIN_SHARP_IZQ A4
+//--------------------SHARP`S-------------------
+//ROSA:     SEÑAL
+//AMARILLO: GND
+//CELESTE:  VCC
 
-int Pines_Sharps[4] = {
-    PIN_SHARP_F_D,
-    PIN_SHARP_F_I,
-    PIN_SHARP_DER,
-    PIN_SHARP_IZQ,
+#define PIN_SHARP_DER A5
+#define PIN_SHARP_IZQ A6
+
+
+unsigned int SHARP_der = 0;
+unsigned int SHARP_izq = 0;
+unsigned int RivalSharp = 450;  // Si no ve nada tende a los 100, Si detecta algo tende a los 900 si està muy cerca. 
+// en cas de que sea un punto promedio e intermedio esta en los 350 y 400.
+
+int Pines_Sharps[2] = {
+  PIN_SHARP_DER,
+  PIN_SHARP_IZQ,
 };
 
-//---------------------CNY70---------------------
-unsigned int CNY70der = 0;
-unsigned int CNY70izq = 0;
-unsigned int BordeTatami = 100;  // NO CAMBIAR DE VALOR
-unsigned int SinSuperficie = 20; // NO CAMBIAR DE VALOR
 
-//--------------VARIABLES DE ENTORNO-------------
+//----------------------JSUMO---------------------
 
+//ROSA:     SEÑAL
+//AMARILLO: GND
+//CELESTE:  VCC
+
+#define PIN_JSUMO_F_D A7
+#define PIN_JSUMO_F_I A4
+
+int sensor =0;
+
+unsigned int JSUMO_der = 0;
+unsigned int JSUMO_izq = 0;
+unsigned int Rival = 500;    //Si no ve nada tira 0 o tende a valores muy cercados. Ej: 17 
+//Si detecta algo, no importa la distancia tende a arriba de lso 800.
+
+int Pines_JSUMO[2] = {
+  PIN_JSUMO_F_D,
+  PIN_JSUMO_F_I,
+};
+
+//---------------------QRE1113--------------------
+
+
+#define QRE_DERECHO A0
+#define QEE_IZQUIERDO A1
+
+unsigned int QREder = 0;
+unsigned int QREizq = 0;
+unsigned int BordeTatami = 500; //Si ve lìnea blanca tende a 200
+unsigned int Tatami = 501;  //Si ve lo negro del tatami tende a 800
 
 
 //-------------------FUNCIONES-------------------
-void Avazar()
-{
-  digitalWrite(MOTOR_DER_1, ON);
-  digitalWrite(MOTOR_DER_2, OFF);
-  digitalWrite(MOTOR_IZQ_1, ON);
-  digitalWrite(MOTOR_IZQ_2, OFF);
-  if (DEBUG)
-    Serial.println("Avanzar");
-}
 
-void Buscar()
-{
-  // DEPENDE LAS ESTRATEGIAS SELECCIONADAS.
-  if (DEBUG)
-    Serial.println("Buscar");
-}
 
-void Derecha()
-{
-  digitalWrite(MOTOR_DER_1, OFF);
-  digitalWrite(MOTOR_DER_2, ON);
-  digitalWrite(MOTOR_IZQ_1, ON);
-  digitalWrite(MOTOR_IZQ_2, OFF);
-  if (DEBUG)
-    Serial.println("Derecha");
-  delay(200);
-}
-
-void Izquierda()
-{
-  digitalWrite(MOTOR_DER_1, ON);
-  digitalWrite(MOTOR_DER_2, OFF);
-  digitalWrite(MOTOR_IZQ_1, OFF);
-  digitalWrite(MOTOR_IZQ_2, ON);
-  if (DEBUG)
-    Serial.println("Izquierda");
-  delay(200);
-}
-
-void AsignacionPinMotores()
-{
-  for (int idx = 0; idx < 4; idx++)
-  {
-    int pin = Pines_Motor[idx];
-    pinMode(pin, OUTPUT);
-  }
-}
-
-void AsignacionPinSharps()
-{
-  for (int idx = 0; idx < 4; idx++)
-  {
-    int pin = Pines_Sharps[idx];
-    pinMode(pin, INPUT);
-  }
-}
-
-void EnviarPulsoUltradonido()
-{
-  digitalWrite(PIN_TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(PIN_TRIG, LOW);
-  Tiempo = pulseIn(PIN_ECHO, HIGH);
-  Distancia = Tiempo / 58;
-}
-void CNY_Der()
-{
-  CNY70der = analogRead(A0);
-
-  if ((CNY70der > SinSuperficie) && (CNY70der < BordeTatami))
-  {
-    // Volver a lo negro
-    if (DEBUG)
-      Serial.println("Me caigo weon");
-  }
-}
-
-void CNY_Izq()
-{
-  CNY70izq = analogRead(A1);
-  if ((CNY70izq > SinSuperficie) && (CNY70izq < BordeTatami))
-  {
-    // Volver a lo negro
-    if (DEBUG)
-      Serial.println("Me caigo weon");
-  }
-}
 
 void EncenderLed() {
   digitalWrite(PIN_LED, HIGH);
@@ -168,111 +114,173 @@ void EncenderLed() {
   delay(350);
 }
 
+void AsignacionPinMotores() {
+  for (int idx = 0; idx < 4; idx++) {
+    int pin = Pines_Motor[idx];
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void AsignacionPinSharps() {
+  for (int idx = 0; idx < 4; idx++) {
+    int pin = Pines_Sharps[idx];
+    pinMode(pin, INPUT);
+  }
+}
+
+void AsignacionPinJSUMO() {
+  for (int idx = 0; idx < 2; idx++) {
+    int pin = Pines_JSUMO[idx];
+    pinMode(pin, INPUT);
+  }
+}
+
+void EnviarPulso() {
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  Tiempo = pulseIn(PIN_ECHO, HIGH);
+  Distancia = Tiempo / 58;
+}
+void Sensar_QRE() {
+  QREder = analogRead(QRE_DERECHO);
+  QREizq = analogRead(QEE_IZQUIERDO);
+  if (QREizq < Tatami || QREder < Tatami){
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, HIGH);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+  digitalWrite(MOTOR_IZQ_1, HIGH);
+  digitalWrite(MOTOR_IZQ_2, LOW);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  delay(300);
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, HIGH);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+  digitalWrite(MOTOR_IZQ_1, LOW);
+  digitalWrite(MOTOR_IZQ_2, HIGH);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  delay(350);
+
+    Serial.println("Me caigo weon");
+  }
+}
+
+void Sensar_Sensores() {
+  JSUMO_izq = analogRead(PIN_JSUMO_F_I);
+  JSUMO_der = analogRead(PIN_JSUMO_F_D);
+  SHARP_der = analogRead(PIN_SHARP_DER);
+  SHARP_izq = analogRead(PIN_SHARP_IZQ);
+}
+
+
 //--------------------SETUP-------------------
-void setup()
-{
-  Serial.begin(9600); // braudate 9600
+void setup() {
+  Serial.begin(9600);  //braudate 9600
   AsignacionPinMotores();
   AsignacionPinSharps();
+  AsignacionPinJSUMO();
   pinMode(PIN_PULSADOR, INPUT);
+  pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(MOTOR_DER_1, OUTPUT);
   pinMode(MOTOR_DER_2, OUTPUT);
   pinMode(MOTOR_IZQ_1, OUTPUT);
   pinMode(MOTOR_IZQ_2, OUTPUT);
-  digitalWrite(PIN_TRIG, LOW); // Talvez Sacar.
-  pinMode(PIN_PULSADOR, INPUT);
-  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_TRIG, LOW);
+  pinMode(PWM_MOTOR_DER, OUTPUT);
+  pinMode(PWM_MOTOR_IZQ, OUTPUT);
+
 }
 
 //--------------------LOOP-------------------
-void loop()
-{
-  
-  pulsador = analogRead(PIN_PULSADOR);
+void loop() {
+    pulsador = analogRead(PIN_PULSADOR);
   delay(200);
 
 
   switch (estado) {
 
     case SET1:
-      if (DEBUG && pantalla == true) {
-        Serial.println("Estrategia 1");
-        EncenderLed();
+      if (pantalla == true) {
+        if(DEBUG)Serial.println("Estrategia 1");
         pantalla = false;
       }
       if (pulsador == LOW) {
         if (millis() >= tiempo_max) {
           estado = ENRUEDO_SET1;
-        } else {
-          estado = SET2;
-          estado_anterior = estado;
-        }
-      }
-      break;
-
-    case SET2:
-      if (DEBUG && pantalla == false) {
-        Serial.println("Estrategia 2");
-        EncenderLed();
-        EncenderLed();
-        pantalla = true;
-      }
-      if (pulsador == LOW) {
-        if (millis() >= tiempo_max) {
-          estado = ENRUEDO_SET2;
-        } else {
-          estado = SET3;
-          estado_anterior = estado;
-        }
-      }
-      break;
-
-    case SET3:
-      if (DEBUG && pantalla == true) {
-        Serial.println("Estrategia 3");
-        EncenderLed();
-        EncenderLed();
-        EncenderLed();
-        pantalla = false;
-      }
-      if (pulsador == LOW) {
-        if (millis() >= tiempo_max) {
-          estado = ENRUEDO_SET3;
-        } else {
-          estado = SET1;
-          estado_anterior = estado;
-        }
       }
       break;
 
     case ENRUEDO_SET1:
       {
-        if (DEBUG) Serial.println("INICIANDO");
-        delay(4700);
-        if (DEBUG) Serial.println("EN RUEDO LA ESTRATEGIA 1");
-        //acá llamar a la función de la estrategia correspondiete
-      }
-      break;
+      if(DEBUG)Serial.println("INICIANDO SET 1");
 
-    case ENRUEDO_SET2:
-      {
-        if (DEBUG) Serial.println("INICIANDO");
-        delay(4700);
-        if (DEBUG) Serial.println("EN RUEDO LA ESTRATEGIA 2");
-        //acá llamar a la función de la estrategia correspondiete
-      }
-      break;
+EnviarPulso();
+Sensar_Sensores();
+//EncenderLed();
 
-    case ENRUEDO_SET3:
-      {
-        if (DEBUG) Serial.println("INICIANDO");
-        delay(4700);
-        if (DEBUG) Serial.println("EN RUEDO LA ESTRATEGIA 3");
-        //acá llamar a la función de la estrategia correspondiete
-      }
-      break;
+
+
+  if(JSUMO_der > RivalSharp){
+  digitalWrite(MOTOR_IZQ_1, HIGH);
+  digitalWrite(MOTOR_IZQ_2, LOW);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  if(DEBUG)Serial.println("JSUMO: LATERAL DERECHA");
   }
 
+  if(JSUMO_izq > RivalSharp){
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, HIGH);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+  if(DEBUG)Serial.println("JSUMO: LATERAL IZQUIERDA");
+  }
+
+  if(JSUMO_der > Rival && Distancia < 30 && JSUMO_izq > Rival){
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, HIGH);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_ATAQUE);
+
+  digitalWrite(MOTOR_IZQ_1, HIGH);
+  digitalWrite(MOTOR_IZQ_2, LOW);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_ATAQUE); 
+  if(DEBUG)Serial.println("Atacar");
+  }else{
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, LOW);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+
+  digitalWrite(MOTOR_IZQ_1, HIGH);
+  digitalWrite(MOTOR_IZQ_2, LOW);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  if(DEBUG)Serial.println("Buscar");
+  }
+
+      
+  if(SHARP_izq > RivalSharp){
+  digitalWrite(MOTOR_DER_1, LOW);
+  digitalWrite(MOTOR_DER_2, HIGH);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+
+  digitalWrite(MOTOR_IZQ_1, LOW);
+  digitalWrite(MOTOR_IZQ_2, HIGH);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  if(DEBUG)Serial.println("SHARP: Rival a la IZQUIERDA");
+  }
+
+  if(SHARP_der > RivalSharp){
+  digitalWrite(MOTOR_DER_1, HIGH);
+  digitalWrite(MOTOR_DER_2, LOW);
+  analogWrite(PWM_MOTOR_DER, VELOCIDAD_BUSCAR);
+
+  digitalWrite(MOTOR_IZQ_1, HIGH);
+  digitalWrite(MOTOR_IZQ_2, LOW);
+  analogWrite(PWM_MOTOR_IZQ, VELOCIDAD_BUSCAR);
+  if(DEBUG)Serial.println("SHARP: Rival a la DERECHA");
+  }
+
+      }
+      break;
+}
+}
 }
