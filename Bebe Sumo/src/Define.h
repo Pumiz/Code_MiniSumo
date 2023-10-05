b865e1451b2ecb1719a52b004b8347bc523def54
Martin Gomez Valle <gomezvallemartin@gmail.com>
2023-10-05 17:09:47 -0300

    Se cambiaron los valores de velocidades para los nuevos motores

--- Bebe Sumo/src/Define.h
+++ Bebe Sumo/src/Define.h
@@ -1,5 +1,5 @@
-#define MOTOR_DER_1 5
-#define MOTOR_DER_2 6
+#define MOTOR_DER_1 6
+#define MOTOR_DER_2 5
 #define MOTOR_IZQ_1 7
 #define MOTOR_IZQ_2 8
 #define PWM_MOTOR_DER 3
@@ -8,13 +8,13 @@
 #define PIN_PULSADOR 4
 #define PIN_LED 13
 
-#define VELOCIDAD_BUSCAR 150
-#define VELOCIDAD_VER_FLAGS 175  //testar velocidad antes de establecer.
-#define VELOCIDAD_ATAQUE 255
-#define VELOCIDAD_ATAQUERAPIDO 180
-#define VELOCIDAD_GHIRAR 230 // Reducir a 215 en caso de que con 11.1V gire muy rápido.
-#define VELOCIDAD_LIMPIAR 200
-#define VELOCIDAD_GHIRARSLOW 60
+#define VELOCIDAD_BUSCAR 70    // 150
+#define VELOCIDAD_VER_FLAGS 80 // 175  //testar velocidad antes de establecer.
+#define VELOCIDAD_ATAQUE 170   // 255
+#define VELOCIDAD_ATAQUERAPIDO 150 // 180
+#define VELOCIDAD_GHIRAR 100 // 230 Reducir si va muy rápido.
+#define VELOCIDAD_LIMPIAR 120   // 200
+#define VELOCIDAD_GHIRARSLOW 30 // 60
 
 #define PIN_TRIG 9
 #define PIN_ECHO 10
@@ -32,12 +32,12 @@ unsigned long Tiempo_acorralar_anterior = 25000;
 unsigned long Tiempo_acorralar_actual = 0;
 int caso = 0;
 unsigned int Tiempo = 0;
-unsigned long Tiempo_Max = 160;  //aprox es 1seg.
+unsigned long Tiempo_Max = 160; // aprox es 1seg.
 unsigned int Tiempo_Min = 20;
 bool Pantalla = true;
 bool Mode = false;
 bool EneableFlags = true;
 bool Visado = false;
 bool Ataque_Derecho = false;
-int Hielo = 1; 
+int Hielo = 1;
 int Oponente = 0;
