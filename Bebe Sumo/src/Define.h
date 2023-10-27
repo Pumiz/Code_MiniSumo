2d506ff589c5c16b1dca709b029c0c956d4bf7f3
Pumiz <gomezvallemartin@gmail.com>
2023-10-26 22:34:58 -0300

    Se cambiaron los pines de los sensores y de los motores.

--- Bebe Sumo/src/Define.h
+++ Bebe Sumo/src/Define.h
@@ -1,28 +1,30 @@
-#define MOTOR_DER_1 5
-#define MOTOR_DER_2 6
+#define MOTOR_DER_1 6
+#define MOTOR_DER_2 5
+
 #define MOTOR_IZQ_1 8
 #define MOTOR_IZQ_2 7
+
 #define PWM_MOTOR_DER 3
 #define PWM_MOTOR_IZQ 11
-
-#define PIN_PULSADOR 4
-#define PIN_LED 13
 
 #define VELOCIDAD_BUSCAR 70    // 150
 #define VELOCIDAD_VER_FLAGS 80 // 175  //testar velocidad antes de establecer.
 #define VELOCIDAD_ATAQUE 170   // 255
 #define VELOCIDAD_ATAQUERAPIDO 150 // 180
-#define VELOCIDAD_GHIRAR 100 // 230 Reducir si va muy rápido.
-#define VELOCIDAD_LIMPIAR 120   // 200
-#define VELOCIDAD_GHIRARSLOW 30 // 60
+#define VELOCIDAD_GHIRAR 100       // 230 Reducir si va muy rápido.
+#define VELOCIDAD_LIMPIAR 80       // 200
+#define VELOCIDAD_GHIRARSLOW 30    // 60
+
+#define PIN_PULSADOR 4
+#define PIN_LED 13
 
 #define PIN_TRIG 9
 #define PIN_ECHO 10
 
-#define PIN_SHARP_DER A5
-#define PIN_SHARP_IZQ A6
+#define PIN_SHARP_DER A6  //A5
+#define PIN_SHARP_IZQ A7  //A6
 
-#define PIN_JSUMO_F_D A7
+#define PIN_JSUMO_F_D A5  //A7
 #define PIN_JSUMO_F_I A4
 
 #define PIN_QRE_DERECHO A0
